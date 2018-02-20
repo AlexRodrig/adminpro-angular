0122a3671cf3437aaceb621cd77579841f26d719
AlexRodrig <alez_1718@live.com>
2018-02-19 21:23:03 -0500

    Input,Output y viewchild

--- src/app/pages/graficas1/graficas1.component.ts
+++ src/app/pages/graficas1/graficas1.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Graficas1Component implements OnInit {
 
+
+  graficos:any = {
+    'grafico1':{
+      'labels':['Con frijoles','Con Natilla', 'Con tocino'],
+      'data':[24,30,46],
+      'type':'doughnut',
+      'leyenda':'El pan se come con'
+    },
+    'grafico2':{
+      'labels':['Hombres','Mujeres'],
+      'data':[4500,6000],
+      'type':'doughnut',
+      'leyenda':'Entrevistados'
+    },
+    'grafico3':{
+      'labels':['SI','NO'],
+      'data':[95,5],
+      'type':'doughnut',
+      'leyenda':'¿Pregunta 1?'
+    },
+    'grafico4':{
+      'labels':['NO','SI'],
+      'data':[85,15],
+      'type':'doughnut',
+      'leyenda':'¿Pregunta 2?'
+    },
+  };
+
   constructor() { }
 
   ngOnInit() {
